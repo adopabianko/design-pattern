eb2d5fcb82c866668437eb745aa01aeb9675ed0f
Ado Pabianko <adopabianko@gmail.com>
2021-07-17 22:45:24 +0700

    feat(abstract factory): add abstract factory design pattern

---
+++ creational-pattern/abstract-factory/main.go
@@ -0,0 +1,165 @@
+package main
+
+import "fmt"
+
+type iSportFactory interface {
+	makeShoe() iShoe
+	makeShirt() iShirt
+}
+
+type adidas struct{}
+
+func (a *adidas) makeShoe() iShoe {
+	return &adidasShoe{
+		shoe: shoe{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
+
+func (a *adidas) makeShirt() iShirt {
+	return &adidasShirt{
+		shirt: shirt{
+			logo: "adidas",
+			size: 14,
+		},
+	}
+}
+
+type nike struct{}
+
+func (n *nike) makeShoe() iShoe {
+	return &nikeShoe{
+		shoe: shoe{
+			logo: "nike",
+			size: 14,
+		},
+	}
+}
+
+func (n *nike) makeShirt() iShirt {
+	return &nikeShirt{
+		shirt: shirt{
+			logo: "nike",
+			size: 14,
+		},
+	}
+}
+
+func getSportsFactory(brand string) (iSportFactory, error) {
+	if brand == "adidas" {
+		return &adidas{}, nil
+	}
+
+	if brand == "nike" {
+		return &nike{}, nil
+	}
+
+	return nil, fmt.Errorf("Wrong brand type passed")
+}
+
+type iShoe interface {
+	setLogo(logo string)
+	setSize(size int)
+	getLogo() string
+	getSize() int
+}
+
+type shoe struct {
+	logo string
+	size int
+}
+
+func (s *shoe) setLogo(logo string) {
+	s.logo = logo
+}
+
+func (s *shoe) getLogo() string {
+	return s.logo
+}
+
+func (s *shoe) setSize(size int) {
+	s.size = size
+}
+
+func (s *shoe) getSize() int {
+	return s.size
+}
+
+type adidasShoe struct {
+	shoe
+}
+
+type nikeShoe struct {
+	shoe
+}
+
+type iShirt interface {
+	setLogo(logo string)
+	setSize(size int)
+	getLogo() string
+	getSize() int
+}
+
+type shirt struct {
+	logo string
+	size int
+}
+
+func (s *shirt) setLogo(logo string) {
+	s.logo = logo
+}
+
+func (s *shirt) getLogo() string {
+	return s.logo
+}
+
+func (s *shirt) setSize(size int) {
+	s.size = size
+}
+
+func (s *shirt) getSize() int {
+	return s.size
+}
+
+type adidasShirt struct {
+	shirt
+}
+
+type nikeShirt struct {
+	shirt
+}
+
+func main() {
+	adidasFactory, _ := getSportsFactory("adidas")
+	nikeFactory, _ := getSportsFactory("nike")
+
+	nikeShoe := nikeFactory.makeShoe()
+	nikeShirt := nikeFactory.makeShirt()
+
+	adidasShoe := adidasFactory.makeShoe()
+	adidasShirt := adidasFactory.makeShirt()
+
+	printShoeDetails(nikeShoe)
+	printShirtDetails(nikeShirt)
+
+	printShoeDetails(adidasShoe)
+	printShirtDetails(adidasShirt)
+}
+
+func printShoeDetails(s iShoe) {
+	fmt.Println("Shoe")
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
+}
+
+func printShirtDetails(s iShirt) {
+	fmt.Println("Shirt")
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
+}
